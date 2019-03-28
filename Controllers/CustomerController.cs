eaa3d57a3fca04cc77d87707c2cd9cc4d3ce984b
BIT student <bitstudent@bitnet.wctc.edu>
2019-03-28 11:36:00 -0500

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private INorthwindRepository repository;
+        public CustomerController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Register() {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Customer model) {
+            if (!ModelState.IsValid) return View();
+            if (repository.Customers.Any(c => c.CompanyName == model.CompanyName))
+            {
+                ModelState.AddModelError("", "Name Must Be Unique");
+
+            }
+            else {
+                repository.AddCustomer(model);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/INorthwindRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Northwind.Models
+{
+    public interface INorthwindRepository
+    {
+        IQueryable<Category> Categories { get; }
+        IQueryable<Product> Products { get; }
+        IQueryable<Discount> Discounts { get; }
+        IQueryable<Customer> Customers { get; }
+
+        void AddCustomer(Customer customer );
+    }
+}
